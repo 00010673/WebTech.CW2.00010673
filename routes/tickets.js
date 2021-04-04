2b24df8c781598cadde26771388d0e9ff480232c
00009115 <wiut00010673@gmail.com>
2021-04-04 19:35:25 +0500

    create functionality

--- routes/tickets.js
+++ routes/tickets.js
@@ -3,10 +3,42 @@ const router = express.Router();
 const fs = require("fs");
 const path = require("path");
 const root = path.dirname(require.main.filename || process.require.main.filename);
-const ticketsDb = path.join(root, "data/dreams.json");
+const ticketsDb = path.join(root, "data/bugs.json");
 
 router.get("/", (req, res) => {
-  res.render("tickets");
+  fs.readFile(ticketsDb, (err, data) => {
+		if (err) throw err;
+
+		const tickets = JSON.parse(data);
+
+		res.render("tickets", { tickets: tickets });
+  });
+});
+
+router.post("/", (req, res) => {
+	const ticket = {
+		name: req.body.name,
+		text: req.body.text,
+		id: id()
+	};
+
+	if (ticket.name.trim() == "" || ticket.text.trim() == "") {
+		res.render("create", { error: true });
+	} else {
+		fs.readFile(ticketsDb, (err, data) => {
+			if (err) throw err;
+
+			const tickets = JSON.parse(data);
+
+			tickets.push(ticket);
+
+			fs.writeFile(ticketsDb, JSON.stringify(tickets), (err) => {
+				if (err) throw err;
+
+				res.render("create", { success: true });
+			});
+		});
+	}
 });
 
 router.get("/create", (req, res) => {
@@ -16,5 +48,9 @@ router.get("/create", (req, res) => {
 router.get("/id", (req, res) => {
   res.render("ticket");
 });
+
+const id = () => {
+	return Math.random().toString(36).substr(2, 9);
+}
 
 module.exports = router;
